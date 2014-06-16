e6b57cfd436af68ffec4afe0896b8f329f97cb5a
john shen <johnshencanada@gmail.com>
2014-06-16 15:07:52 -0700

    Initial Commit

---
+++ SnapChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapChat
+//
+//  Created by john on 6/16/14.
+//  Copyright (c) 2014 Banana Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnapChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SnapChat
+//
+//  Created by john on 6/16/14.
+//  Copyright (c) 2014 Banana Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
